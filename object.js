0c8366083b270cff9d3da468748bf9a5bd208a67
narasimhasunil <konathala.narasimha@gmail.com>
2021-05-29 11:46:17 +0530

    ob

---
+++ object.js
@@ -0,0 +1,18 @@
+const employeeData = {
+    firstname: 'narasimha',
+    lastname: 'konathala',
+    age: 28,
+    height: 173,
+}
+
+//console.log(employeeData);
+
+employeeData.workex = 3;
+
+//console.log(employeeData);
+
+//console.log(` ${employeeData.firstname} ${employeeData.lastname}`);
+
+delete employeeData.firstname;
+
+console.log(employeeData);
