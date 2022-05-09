2100793768f7c398eb4b6b9987d8761d93041475
narasimhasunil <konathala.narasimha@gmail.com>
2022-05-09 11:20:50 +0530

    data type

---
+++ dataTypes.js
@@ -0,0 +1,20 @@
+//4 types of data types
+// 1. number
+// 2. string
+// 3. boolean
+// 4. null
+// 5. undefined
+
+
+const studentRollNum = [154, 173, 165, 158, 162, 168];
+
+const studentName = ['James, ben, sam, tom, jerry, robert'];
+
+const isPresent = true;
+
+const totalpresent = null;
+
+const address = undefined;
+
+let result;
+console.log(typeof studentName);
---
+++ percentageProgram.js
@@ -0,0 +1,5 @@
+const numberOne = 10
+const numberTwo = 50
+let result;
+result = numberOne % numberTwo;
+console.log(result);
