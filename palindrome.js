130aa33cf5e2a2399b25682a9574e51b3feb8fe2
narasimhasunil <konathala.narasimha@gmail.com>
2022-05-10 08:08:17 +0530

    palindrome

---
+++ palindrome.js
@@ -0,0 +1,11 @@
+const name1 = 'tenet';
+
+//to find out the given variable is palindrome or not
+//1st split the string
+//2nd reverse the string
+//then join the string
+//finally compaare the result and initial variable
+
+const name1Split = name1.split('');
+const name1Reversed = name1Split.reverse();
+const joinReversedName = name1Reversed.join('');
