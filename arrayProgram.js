2c270c7d8e9fa94b5967af9418adbd0753e248d9
narasimhasunil <konathala.narasimha@gmail.com>
2022-05-10 07:46:31 +0530

    array sample

--- arrayProgram.js
+++ arrayProgram.js
@@ -1,5 +1,19 @@
 //an array is used to store multiple values in single variable
 
-const scoresOfPlayers = [21,0,1,34,45,0];
+const scoresOfPlayers = [2,51,6,12,13,15];
 
-console.log(typeof scoresOfPlayers);
+console.log('type : ', typeof scoresOfPlayers);
+
+console.log('size :  ', scoresOfPlayers.length);
+
+const namesOfPlayers = ['rohit','kishan','sky','tilak','david','pollard'];
+
+console.log(namesOfPlayers [4]);
+
+namesOfPlayers[2] = 'tSingh';
+console.log(namesOfPlayers);
+
+const namesOfPlayers2 = ['sams', 'ashwin', 'kartikeya', 'bumrah', 'meredith'];
+
+console.log(allPlayerNames = namesOfPlayers.concat(namesOfPlayers2));
+
