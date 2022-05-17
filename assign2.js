11689f0a1c07ec6fd401b5ff70c78f593e7cd455
narasimhasunil <konathala.narasimha@gmail.com>
2022-05-17 20:09:36 +0530

    ass

---
+++ assign2.js
@@ -0,0 +1,16 @@
+const str = 'prabhavathi';
+
+let rep = str.replace('prabhavathi','kalavathi');
+
+console.log(rep);
+
+
+rep = 'prabhavathi';
+console.log(rep);
+
+let res1 = rep.slice(7);
+console.log(res1);
+
+const rep2 = 'kalav';
+
+console.log(res2 = rep2.concat(res1));
---
+++ assignFunc.js
@@ -0,0 +1,15 @@
+const res = sub (3, 5);
+console.log(res);
+const res2 = sub (7, 9);
+console.log(res2);
+
+const res3 = sub (23, 16);
+console.log(res3);
+
+function sub (a, b) {
+    const y = b-a;
+    return y;
+ //console.log(y);
+}
+
+
