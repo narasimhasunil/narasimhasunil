4e3f5b0e94ebba30a9fbaeb85cddda69883f3249
narasimhasunil <konathala.narasimha@gmail.com>
2022-05-17 20:09:16 +0530

    assign

---
+++ array/arrayOperations.js
@@ -0,0 +1,12 @@
+const arr = [23, 43, 55, 67, 89, 91];
+
+arr.push(101); //push command is used to add a new element into the array
+console.log(arr); 
+
+arr.pop(); //pop command is used to remove an element from last index position from an array
+console.log(arr);
+
+console.log(arr.join(''));
+
+arr2 = [23,34,45,67,89];
+console.log(arr2.shift()); //shift
