f30343c8c4da8d423acb65ae2562d5d6ded96863
narasimhasunil <konathala.narasimha@gmail.com>
2022-05-28 21:30:23 +0530

    find

---
+++ find.js
@@ -0,0 +1,11 @@
+const abc = [12, 23, 34, 56, 'mounish', 'sunil'];
+
+const xyz = abc.find( x => x > 30);
+
+console.log(xyz);
+
+const abc2 = [23, 34, 56, 78, 99];
+
+const rs = value => value > 100;
+
+console.log(abc2.findIndex(rs));
--- map.js
+++ map.js
@@ -9,3 +9,13 @@ console.log(res);
 const res2 = res.map(x => x * 2);
 
 console.log(res2);
+
+const samp2 = [2, 3, 4];
+
+const res3 = samp2.map( num => num*num );
+
+console.log(res3);
+
+const res4 = res3.map( x => Math.sqrt(x));
+
+console.log(res4);
