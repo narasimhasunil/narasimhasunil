1ec74fcf4a61867f21ef7bffffaf51843235c266
narasimhasunil <konathala.narasimha@gmail.com>
2022-05-30 20:28:39 +0530

    table

---
+++ table.js
@@ -0,0 +1,21 @@
+const sampleTable = [
+    { firstname : 'sunil', lastname : 'narasimha'},
+    { firstname : 'mounish', lastname : 'krishna'},
+    { firstname : 'seshu', lastname : 'sai'}
+];
+for (i= 0;i< sampleTable.length;i=i+1){
+    sampleTable[i].workexp = i;
+}
+console.table(sampleTable);
+
+
+//sampleTable[0].workEx = 2
+//sampleTable[1].workEx = 4;
+
+//console.table(sampleTable);
+
+
+
+
+
+
