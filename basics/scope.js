03153bdc1623d7c7af78552eebb7ae353380c447
narasimhasunil <konathala.narasimha@gmail.com>
2022-06-11 16:47:34 +0530

    sam

--- basics/scope.js
+++ basics/scope.js
@@ -1,7 +1,7 @@
 nam ('sunil');
 function nam () {
     if (12 % 2 === 0) {
-        console.log(' entered into block');
+        console.log('entered into block');
         let x  = 45;
     }
     console.log(x);
