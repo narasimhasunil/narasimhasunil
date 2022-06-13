633bd2a71e8ed402fb861908f84e25ca2c0efaca
narasimhasunil <konathala.narasimha@gmail.com>
2022-06-13 19:53:56 +0530

    class

---
+++ class/class1.js
@@ -0,0 +1,36 @@
+// const obj = {
+//     name:'Ram',
+//     age:23,
+//     fullName : function () {
+//         return this.age+this.name
+//     }
+// }
+
+// console.log(obj.fullName());
+
+// ES5 syntax
+class Sample {
+    constructor(name, age){ // used for initializing the variables
+        this.name = name;
+        this.age = age;
+    }
+
+    fullName () {
+        return `${this.name} ${this.age}`
+    }
+
+    fullName2 () {
+        return `${this.name} ${this.age}`
+    }
+
+    static staticMethod() {
+        return 'static method has been called.';
+    }
+}
+
+const obj = new Sample('ram', 29);
+const obj2 = new Sample('shyam', 229);
+console.log(obj.fullName());
+console.log(obj2.fullName());
+
+console.log(Sample.staticMethod());
---
+++ class/importclass.js
@@ -0,0 +1,5 @@
+import { StudentDetails } from "./class2";
+
+const obj3 = new StudentDetails ('mounish', 33, 173, 'M', 98);
+
+console.log(obj3);
