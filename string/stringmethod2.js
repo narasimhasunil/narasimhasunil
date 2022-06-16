3fc284f24de461256e9ea1ca8f2c7044bc9141d1
narasimhasunil <konathala.narasimha@gmail.com>
2022-06-16 20:35:28 +0530

    st

---
+++ string/stringmethod2.js
@@ -0,0 +1,19 @@
+const sample = 'Narasimha';
+
+console.log(sample.slice(4));
+
+console.log(sample.slice(4,7));
+
+const exm1 = 'Ram Rahim Rober';
+
+console.log(exm1.replace('Rober','Rehman'));
+
+const exm2 = '       Ram Rahim Rober';
+
+const exm3 = 123490;
+
+const exm4 = exm3.toString();
+
+console.log(exm4);
+
+console.log(typeof exm4);
